afda515b4cb483d014311d4c9a5507bfa05d407d
Obinna <oisiwekpeni@gmail.com>
2019-11-15 01:03:44 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>A TO-DO LIST APPLICATION</h1>
+    <Activity/>
+  </div>
+</template>
+
+<script>
+import Activity from './components/Activity.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Activity
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 150px;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+h1{
+  text-align: center;
+}
+</style>
---
+++ src/components/Activity.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <form @submit.prevent="addTask">
+        <input type="text" placeholder="Enter your task for today" v-model="task">
+      </form>
+
+      <ul>
+        <li v-for="(data,index) in tasks" :key="index">{{data}}
+          <button @click="removeTask(index)">Delete</button>
+        </li>
+        
+      </ul>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Activity',
+  data(){
+    return{
+      task : '',
+      tasks: []
+    }
+  },
+  methods:{
+    addTask(){
+      this.tasks.push(this.task)
+      this.task = ''
+    },
+    removeTask(id){
+      this.tasks.splice(id,1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+
+.holder {
+    background: #fff;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+   input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
+</style>
