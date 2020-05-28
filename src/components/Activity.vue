0e9451fb998c586ff25541194fabb6c0369febe1
obinnadtq <oisiwekpeni@gmail.com>
2020-05-28 02:04:56 +0200

    added logic for adding task

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -1,40 +1,58 @@
 <template>
-  <div class="hello">
-    <div class="holder">
-      <form @submit.prevent="addTask">
-        <input type="text" placeholder="Enter your task for today" v-model="task">
-      </form>
+  <div>
+    <input type="text" class="todo-input" v-model="newTodo" @keyup.enter="addTodo" placeholder="Enter your todos">
+    <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+    <div v-for="todo in todosFiltered" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <input type="checkbox" v-model="todo.completed">
+        <div>{{todo.title}}</div>
+      </div>
 
-      <ul>
-        <li v-for="(data,index) in tasks" :key="index">{{data}}
-          <button @click="removeTask(index)">Delete</button>
-        </li>
-        
-      </ul>
     </div>
-    
+    </transition-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Activity',
-  data(){
-    return{
-      task : '',
-      tasks: []
+  name: "Activity",
+  data() {
+    return {
+      todos: [],
+      newTodo: "",
+      idForTodo: 1,
+      filter: "all"
+    };
+  },
+  methods: {
+    // create function to add todo
+    addTodo() {
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+        editing: false
+      });
+      this.newTodo = "";
+      this.idForTodo++;
     }
   },
-  methods:{
-    addTask(){
-      this.tasks.push(this.task)
-      this.task = ''
-    },
-    removeTask(id){
-      this.tasks.splice(id,1)
+  computed: {
+    // create computed property for all , active and completed todos
+    todosFiltered() {
+      if (this.filter === "all") {
+        return this.todos;
+      }
+      else if(this.filter === "active") {
+        return this.todos.filter(todo => !todo.completed)
+      }
+      else if(this.filter === "completed") {
+        return this.todos.filter(todo => todo.completed)
+      }
+      return this.todos;
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -42,31 +60,21 @@ export default {
 @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
-.holder {
-    background: #fff;
-  }
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  }
-  
-  ul li {
-    padding: 20px;
-    font-size: 1.3em;
-    background-color: #E0EDF4;
-    border-left: 5px solid #3EB3F6;
-    margin-bottom: 2px;
-    color: #3E5252;
-  }
+.todo-input{
+  width: 100%;
+  font-size: 18px;
+  padding: 10px 18px;
+  margin-bottom: 16px;
+}
 
-   input {
-    width: calc(100% - 40px);
-    border: 0;
-    padding: 20px;
-    font-size: 1.3em;
-    background-color: #323333;
-    color: #687F7F;
-  }
+.todo-item{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
+.todo-item-left{
+  display: flex;
+  align-items: center;
+}
 </style>
